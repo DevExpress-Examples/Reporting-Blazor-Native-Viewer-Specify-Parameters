97cecdfead5d0c4ee8d43a11a7814553493c28fa
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2021-10-22 19:25:39 +0300

    auto sync for version 2021.2

--- CS/BlazorApp/Startup.cs
+++ CS/BlazorApp/Startup.cs
@@ -23,8 +23,6 @@ namespace BlazorApp {
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddDevExpressServerSideBlazorReportViewer();
-            services.AddControllers(options => options.EnableEndpointRouting = false)
-                .AddDevExpressServerSideBlazorReportViewerControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
