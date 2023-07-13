4cdb5dc6900e484c1cb180d68134fe783bc4b414
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2023-07-13 17:21:32 +0400

    auto sync for version 2023.1

--- CS/BlazorApp/Startup.cs
+++ CS/BlazorApp/Startup.cs
@@ -40,8 +40,6 @@ namespace BlazorApp {
                 app.UseHsts();
             }
 
-            app.UseDevExpressServerSideBlazorReportViewer();
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
